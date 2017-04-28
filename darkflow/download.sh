d19eb621bcd9f43b0c0bca36dceaabe7d7efecbc
Loreto Parisi <loretoparisi@gmail.com>
2017-04-28 16:51:55 +0200

    Docker download container file script

--- darkflow/download.sh
+++ darkflow/download.sh
@@ -2,7 +2,8 @@
 
 IMAGE=darkflow-gpu
 VIDEO=video.avi
+VIDEO_OUT=$1
 
 CONTAINER_ID=$(docker ps -a | grep $IMAGE | awk '{print $1}')
-echo Downloading $VIDEO from $CONTAINER_ID...
-docker cp $CONTAINER_ID:/darkflow/darkflow/$VIDEO ./$VIDEO
+echo Downloading $VIDEO from $CONTAINER_ID as $VIDEO_OUT...
+docker cp $CONTAINER_ID:/darkflow/darkflow/$VIDEO $VIDEO_OUT
