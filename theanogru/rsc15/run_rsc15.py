b1f304113a5c4d79446b484d1c155a7c7e142464
Loreto Parisi <loretoparisi@gmail.com>
2017-07-25 18:31:04 +0200

    theano: fixes to train script

--- theanogru/rsc15/run_rsc15.py
+++ theanogru/rsc15/run_rsc15.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     
     #Reproducing results from "Session-based Recommendations with Recurrent Neural Networks" on RSC15 (http://arxiv.org/abs/1511.06939)
     
-    print('Training GRU4Rec with ' + LAYERS + ' hidden units')    
+    print('Training GRU4Rec with ' + str(LAYERS) + ' hidden units')    
     
     gru = gru4rec.GRU4Rec(loss='top1', final_act='tanh', hidden_act='tanh', layers=[LAYERS], batch_size=50, dropout_p_hidden=0.5, learning_rate=0.01, momentum=0.0, time_sort=False)
     gru.fit(data)
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     
     #Reproducing results from "Recurrent Neural Networks with Top-k Gains for Session-based Recommendations" on RSC15 (http://arxiv.org/abs/1706.03847)
     
-    print('Training GRU4Rec with 100 hidden units')
+    print('Training GRU4Rec with ' + str(LAYERS) + ' hidden units')   
 
     gru = gru4rec.GRU4Rec(loss='bpr-max-0.5', final_act='linear', hidden_act='tanh', layers=[LAYERS], batch_size=32, dropout_p_hidden=0.0, learning_rate=0.2, momentum=0.5, n_sample=2048, sample_alpha=0, time_sort=True)
     gru.fit(data)
