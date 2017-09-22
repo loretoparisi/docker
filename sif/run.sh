2d0782eec67ea110ab4469d3ca39831b4a357f6f
Loreto Parisi <loretoparisi@gmail.com>
2017-09-22 14:57:23 +0200

    darknet: fixed cpu settings

--- sif/run.sh
+++ sif/run.sh
@@ -8,5 +8,5 @@ CMD=./demo.sh
 else
 CMD=bash
 fi
-# execute docker run with nvidia driver and device
-docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 --volume-driver nvidia-docker -v nvidia_driver_367.57:/usr/local/nvidia:ro $IMAGE $CMD
+
+docker run -it $IMAGE $CMD
