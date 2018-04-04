613a72ccd952c27887eb70411bc7c2435e8676c4
Loreto Parisi <loretoparisi@gmail.com>
2018-04-04 12:43:02 +0200

    G2P: added build file

---
+++ g2p-seq2seq/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# G2P TensorFlow
+# @author Loreto Parisi (loretoparisi at gmail dot com)
+# v1.0.0
+# @2018 Loreto Parisi (loretoparisi at gmail dot com)
+#
+
+docker build -t g2p-seq2seq .
