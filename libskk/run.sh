a2d458bef2861fcbb5c8d736f69fee4998923bb1
Loreto Parisi <loretoparisi@gmail.com>
2018-09-26 17:27:19 +0200

    Added libskk dockerfile

---
+++ libskk/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# indic-trans  CPU Run Script
+# @author Loreto Parisi (loretoparisi at gmail dot com)
+# v1.0.0
+# @2018 Loreto Parisi (loretoparisi at gmail dot com)
+#
+IMAGE=libskk
+docker run --rm -it $IMAGE bash
